bf1b4d1da5a9f018e43c7baf9222e8ad2228fb98
Ilya030290 <anoshko90@mail.ru>
2022-05-18 15:55:32 +0300

    New styles for Main, Header and Skills components

--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -5,14 +5,18 @@ import {Skill} from "./Skill/Skill";
 
 export const Skills = () => {
     const mySkills = [
-        {title: 'HTML, CSS', description: 'Lorem ipsum, some asdsadsadsds sdsdsadasd sdaasdasda text for skill html, css', icon: ''},
-        {title: 'JS', description: 'Lorem ipsum, some text sdfsdfdsfdsfdsfsadsadsad for skill javascript', icon: ''},
-        {title: 'React', description: 'some text asdsadsadsds sdsdsadasd sdaasdasda for skill react', icon: ''}
+        {title: 'HTML', description: 'Lorem ipsum, some asdsadsadsds sdsdsadasd sdaasdasda text for skill html, css', icon: ''},
+        {title: 'CSS', description: 'Lorem ipsum, some text sdfsdfdsfdsfdsfsadsadsad for skill css', icon: ''},
+        {title: 'JavaScript', description: 'some text asdsadsadsds sdsdsadasd sdaasdasda for skill javascript', icon: ''},
+        {title: 'Redux', description: 'some text asdsadsadsds sdsdsadasd sdaasdasda for skill redux', icon: ''},
+        {title: 'React', description: 'some text asdsadsadsds sdsdsadasd ssda for skill react', icon: ''}
     ];
     return (
         <div className={style.skillsBlock}>
             <div className={`${styleContainer.container}  ${style.skillsContainer}`}>
-                <h2 className={style.title}>Skills</h2>
+                <div className={style.title}>
+                    <h2>My <span>Skills</span></h2>
+                </div>
                 <div className={style.skills}>
                     {
                         mySkills.map((el, id) =>
