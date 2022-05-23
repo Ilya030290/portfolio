0784c9b88ea1e8bcd1a1651253bca16c525f2fe4
Ilya030290 <anoshko90@mail.ru>
2022-05-23 14:42:08 +0300

    Styles for main component, contacts component

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -2,21 +2,25 @@ import React from 'react';
 import style from './Contacts.module.scss';
 import styleContainer from "../common/styles/Container.module.css";
 
+
 export const Contacts = () => {
+
     return (
         <div className={style.contactsBlock}>
             <div className={`${styleContainer.container} ${style.contactsContainer}`}>
                 <div className={style.title}>
                     <h2>My <span>Contacts</span></h2>
                 </div>
-                <div className={style.mail}>
-                    <h2>Mail:</h2>
-                    <span>https://anoshko90@mail.ru</span>
-                    <span>https://anoshko9079@gmail.com</span>
-                </div>
-                <div className={style.phone}>
-                    <h2>Phone:</h2>
-                    <span>+375292196011</span>
+                <div className={style.mailPhoneContainer}>
+                    <div className={style.mail}>
+                        <h2>Mail</h2>
+                        <span>anoshko9079@gmail.com</span>
+                        <span>anoshko90@mail.ru</span>
+                    </div>
+                    <div className={style.phone}>
+                        <h2>Phone</h2>
+                        <span>+375292196011</span>
+                    </div>
                 </div>
             </div>
         </div>
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import style from './Main.module.css';
 import styleContainer from '../common/styles/Container.module.css';
+import mainPhoto from '../assets/mainPhoto/220190035_882950875638101_810745229767205643_n.jpg';
 
 export const Main = () => {
+
+    const photo = {
+        backgroundImage: `url(${mainPhoto})`
+    }
+
     return (
         <div className={style.mainBlock}>
             <div className={styleContainer.container}>
@@ -11,7 +17,7 @@ export const Main = () => {
                     <h1><span>I am</span> Ilya Anoshko</h1>
                     <p>Frontend Developer</p>
                 </div>
-                <div className={style.photo}></div>
+                <div className={style.photo} style={photo}></div>
             </div>
         </div>
     );
